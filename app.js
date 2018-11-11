25f8f7a7de7100378a338d24f0c5410446d3e2d2
Preetam rao <raopreetam007@gmail.com>
2018-11-11 12:06:32 +0530

    nocache npm module added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require('express')
 var path=require('path');
+const nocache = require('nocache');
 
 const app=express();
 app.use(express.static(path.join(__dirname,'build')));
+app.use(nocache());
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'build/index.html'));
 
